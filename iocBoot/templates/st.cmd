addd9ca7e828541f7c9a2f50ae6bc4918babb612
slactjohnson <tjohnson@slac.stanford.edu>
2024-05-02 14:41:24 -0700

    Setup IOC to use new udev rule for TEM

--- iocBoot/templates/st.cmd
+++ iocBoot/templates/st.cmd
@@ -22,11 +22,7 @@ TEM_PhaseLock_registerRecordDeviceDriver(pdbbase)
 ## Set up IOC/hardware links -- LAN connection
 ##############################################################
 $$LOOP(DEVICE)
-$$IF(HOST)
-drvAsynIPPortConfigure( "bus$$INDEX", "$$HOST:$$PORT", 0, 0, 0 )
-$$ELSE(HOST)
-drvAsynSerialPortConfigure( "bus$$INDEX", "/dev/$$PORT", 0, 0, 0 )
-$$ENDIF(HOST)
+drvAsynSerialPortConfigure( "bus$$INDEX", "/dev/usbTEM$$N", 0, 0, 0 )
 $$IF(ASYNTRACE)
 asynSetTraceMask( "bus$$INDEX", 0, 0x09 )
 asynSetTraceIOMask( "bus$$INDEX", 0, 0x0 )
